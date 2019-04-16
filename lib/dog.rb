739acc2a7f25f9ddab77aa8c5fb583d125618733
Rachele Suy <rachelesuy@gmail.com>
2019-04-16 16:22:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Dog
+  
+  def name=(name)
+    @name =name
+  end
+  
+   def name 
+    @name 
+  end
+  
+  def bark
+    puts "Woof!"
+  end
+end
+  fido = Dog.new 
+  fido.name = ("Fido")
+  #fido.bark
